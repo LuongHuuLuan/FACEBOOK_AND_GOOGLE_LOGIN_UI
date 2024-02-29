0b04fd987fac836632ed6662795e092f4d50e967
LuongHuuLuan <luongluanmpt@gmail.com>
2024-02-29 14:54:17 +0700

    fix secrec

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,13 +1,16 @@
 import NextAuth from "next-auth/next";
 import FacebookProvider from 'next-auth/providers/facebook';
 
+
+console.log(process.env.FACEBOOK_ID)
 const authOptions = {
     providers: [
         FacebookProvider({
             clientId: process.env.FACEBOOK_ID ||"",
             clientSecret: process.env.FACEBOOK_SECRET || ""
         })
-    ]
+    ],
+    secret: process.env.SECRET
 }
 
 const handler = NextAuth(authOptions)
